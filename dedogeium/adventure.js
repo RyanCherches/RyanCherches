68ba7fe8f2edd500832fb46b63928530bac1a367
RyanCherches <rfcherches@mcdonogh.org>
2026-01-25 16:09:52 -0500

    adds different doges to fight in adventure(you still can't fight them), and adds more doges to run into battle when you hover over the picture of doge.

--- dedogeium/adventure.js
+++ dedogeium/adventure.js
@@ -1,9 +1,16 @@
 const dogeAttack = document.getElementById("doge-attack");
+const dogeAttack2 = document.getElementById("doge-attack2");
+const dogeAttack3 = document.getElementById("doge-attack3");
 let isHovering = false;
 let offsetX = 0;
+let offsetY3 = 0;
 
 dogeAttack.addEventListener("mouseenter", () => {
   dogeAttack.src = "doge attack.png";
+  dogeAttack2.src = "doge attack.png";
+  dogeAttack2.style.display="block";
+  dogeAttack3.src = "doge attack.png";
+  dogeAttack3.style.display="block";
   isHovering = true;
   
   const moveInterval = setInterval(() => {
@@ -12,14 +19,23 @@ dogeAttack.addEventListener("mouseenter", () => {
       return;
     }
     offsetX += 1;
+    offsetY3 += 0.1;
     dogeAttack.style.transform = `translate(${offsetX}px)`;
+    dogeAttack2.style.transform = `translate(${offsetX}px)`;
+    dogeAttack3.style.transform = `translate(${offsetX}px, ${offsetY3}px)`;
   }, 1);
 });
 
 dogeAttack.addEventListener("mouseleave", () => {
   dogeAttack.src = "Im just a chill guy no background.png";
+  dogeAttack2.src = "Im just a chill guy no background.png";
+  dogeAttack2.style.display="none";
+  dogeAttack3.src = "Im just a chill guy no background.png";
+  dogeAttack3.style.display="none";
   isHovering = false;
   offsetX = 0;
-  offsetY = 0;
+  offsetY3 = 0;
   dogeAttack.style.transform = `translate(0, 0)`;
+  dogeAttack2.style.transform = `translate(0, 0)`;
+  dogeAttack3.style.transform = `translate(0, 0)`;
 });

73374b0c18ba073783a75378503d4dff99931de8
RyanCherches <rfcherches@mcdonogh.org>
2026-03-28 17:02:27 -0400

    adds a google form that if you want to fill you can. Also adds a little of tutorial.

--- dedogeium/index.js
+++ dedogeium/index.js
@@ -2,3 +2,4 @@ let adminLog = document.getElementById("admin-log");
 adminLog.addEventListener("click", function() {
     window.location.href = "adminLogin.html";
 });
+alert("If you would like to help support dedogeium, please fill this form out so that I can get your opinion on pay to win items: https://docs.google.com/forms/d/e/1FAIpQLScKW--yYulXwAcMJLv6AKbJmaC8CNUnSE7OQlA8x3CSG1Z22Q/viewform?usp=publish-editor")
---
+++ dedogeium/tutorial.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const CharacterMain = document.getElementById("main-character");
+    const Spotlight1 = document.getElementById("spotlight");
+    welcome(CharacterMain, Spotlight1);
+});
+function welcome(CharacterMain, Spotlight1) {
+    setTimeout(function () {
+        alert("Welcome to DeDogeium! Click OK to begin you tutorial!");
+        const continueTutorial = prompt("would you like to continue with the tutorial? (yes or no)");
+        const answer = (continueTutorial || "").trim().toLowerCase();
+        if (answer === "yes") {
+            alert("Press ok to move on, and if you want to skip the tutorial, then go back to adventure.html and click the first level to begin.");
+            mainPlayers(CharacterMain, Spotlight1);
+        } else {
+            window.location.href = "adventure.html";
+        }
+    }, 50);
+}
+function mainPlayers(CharacterMain, Spotlight1) {
+    CharacterMain.style.display = "block";
+    Spotlight1.style.display = "block";
+    setTimeout(function () {
+        alert("This is your character. As you move on, you will get more items to upgrade your doge.")
+    }, 50);
+}
